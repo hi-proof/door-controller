f4ce8b398ea7afd93ec1e36ef399f25c79e36a99
Ryan Williamson <ryanwilliamson.66@gmail.com>
2019-11-17 16:26:29 -0800

    Add stubbed files for call panel implementation

---
+++ elevator/call_panel.cpp
@@ -0,0 +1,8 @@
+/** Implementation details for the call panel hardware
+ * 
+ */
+
+#include "call_panel.h"
+
+using namespace hiproof::elevator;
+
---
+++ elevator/call_panel.h
@@ -0,0 +1,20 @@
+/** Interface specification for the call panel
+ * 
+ * @todo Question whether we need two classes for the two panels
+ * 
+ */
+
+#pragma once
+
+#ifndef HI_PROOF_CALL_PANEL_H_
+#define HI_PROOF_CALL_PANEL_H_
+
+namespace hiproof {
+namespace elevator {
+
+class CallPanel final {};
+
+} // elevator
+} // hiproof
+
+#endif // HI_PROOF_CALL_PANEL_H_
