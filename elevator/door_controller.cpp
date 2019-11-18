fdd8252a9f7a91b498c7c1b1f6f46fe4bc9234b7
Ryan Williamson <ryanwilliamson.66@gmail.com>
2019-11-17 18:46:16 -0800

    Populate some more of the door code

--- elevator/door_controller.cpp
+++ elevator/door_controller.cpp
@@ -1,9 +1,13 @@
 /** Implementation details for door controller
- * 
+ *
  */
 
 #include "door_controller.h"
 
 using namespace hiproof::elevator;
 
+DoorController::DoorController(const int step_pin, const int dir_pin,
+                               const int home_pin, const int overrun_pin) {}
 
+void enterSafeMode() {}
+void emergencyStop() {}
--- elevator/door_controller.h
+++ elevator/door_controller.h
@@ -1,7 +1,8 @@
 /** Interface specs for a single door controller
- * 
+ *
  * @todo Will need some syncronization systems
- * 
+ * @todo realistically most of this code is the same as the floor, but like.. eh..
+ *
  */
 
 #pragma once
@@ -14,9 +15,18 @@
 namespace hiproof {
 namespace elevator {
 
-class DoorController final : SafetyCriticalComponent {};
+class DoorController final : SafetyCriticalComponent {
+ public:
+  DoorController(const int step_pin, const int dir_pin, const int home_pin,
+                 const int overrun_pin);
+  ~DoorController(){};
+  void enterSafeMode();
+  void emergencyStop();
 
-} // elevator
-} // hiproof
+ private:
+};
 
-#endif // HI_PROOF_DOOR_CONTROLLER_H_
+}  // namespace elevator
+}  // namespace hiproof
+
+#endif  // HI_PROOF_DOOR_CONTROLLER_H_
