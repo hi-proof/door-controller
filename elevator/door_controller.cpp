dd05de7d8e816b66607b02436157e9c1f55cedbe
Ryan Williamson <r25willi@edu.uwaterloo.ca>
2019-11-18 21:02:44 -0800

    Move some of the floor code to the door controller

--- elevator/door_controller.cpp
+++ elevator/door_controller.cpp
@@ -6,8 +6,34 @@
 
 using namespace hiproof::elevator;
 
-DoorController::DoorController(const int step_pin, const int dir_pin,
-                               const int home_pin, const int overrun_pin) {}
+DoorController::DoorController(const int d1_step_pin, const int d1_dir_pin,
+                               const int d1_home_pin, const int d1_overrun_pin,
+                               const int d2_step_pin, const int d2_dir_pin,
+                               const int d2_home_pin, const int d2_overrun_pin)
+    : left_door_{.stepper = Stepper(d1_step_pin, d1_step_pin),
+                 .home_pin = Bounce(),
+                 .overrun_pin = Bounce()},
+      right_door_{.stepper = Stepper(d2_step_pin, d2_step_pin),
+                  .home_pin = Bounce(),
+                  .overrun_pin = Bounce()} {
+  left_door_.home_pin.attach(d1_home_pin, INPUT_PULLUP);
+  left_door_.home_pin.interval(kDebounceInterval);
+  left_door_.overrun_pin.attach(d1_overrun_pin, INPUT_PULLUP);
+  left_door_.overrun_pin.interval(kDebounceInterval);
+  right_door_.home_pin.attach(d2_home_pin, INPUT_PULLUP);
+  right_door_.home_pin.interval(kDebounceInterval);
+  right_door_.overrun_pin.attach(d2_overrun_pin, INPUT_PULLUP);
+  right_door_.overrun_pin.interval(kDebounceInterval);
+}
 
-void enterSafeMode() {}
-void emergencyStop() {}
+void DoorController::enterSafeMode() {
+  rotate_controller.stop();
+  step_controller.stop();
+  open();
+  safe_ = true;
+}
+
+void DoorController::emergencyStop() {
+  step_controller.emergencyStop();
+  rotate_controller.emergencyStop();
+}
--- elevator/door_controller.h
+++ elevator/door_controller.h
@@ -1,7 +1,8 @@
 /** Interface specs for a single door controller
  *
  * @todo Will need some syncronization systems
- * @todo realistically most of this code is the same as the floor, but like.. eh..
+ * @todo realistically most of this code is the same as the floor, but like..
+ * eh..
  *
  */
 
@@ -11,20 +12,46 @@
 #define HI_PROOF_DOOR_CONTROLLER_H_
 
 #include "safety_critical_component.h"
+#include "TeensyStep.h"
+#include "Bounce2.h"
 
 namespace hiproof {
 namespace elevator {
 
 class DoorController final : SafetyCriticalComponent {
  public:
-  DoorController(const int step_pin, const int dir_pin, const int home_pin,
-                 const int overrun_pin);
+  DoorController(const int d1_step_pin, const int d1_dir_pin,
+                 const int d1_home_pin, const int d1_overrun_pin,
+                 const int d2_step_pin, const int d2_dir_pin,
+                 const int d2_home_pin, const int d2_overrun_pin);
   ~DoorController(){};
   void enterSafeMode();
   void emergencyStop();
 
+  void open();
+  void close();
+
  private:
-};
+  static constexpr uint16_t kHomingSpeed = 1000;
+  static constexpr uint16_t kHomingAccel = 1000;
+  static constexpr uint16_t kMaxLiveSpeed = 1500;
+  static constexpr uint16_t kMaxLiveAccel = 500;
+  static constexpr uint16_t kDebounceInterval = 25;
+
+  using DoorData = struct {
+    Stepper stepper;
+    Bounce home_pin;
+    Bounce overrun_pin;
+  };
+
+  DoorData left_door_;
+  DoorData right_door_;
+  uint16_t run_speed{kMaxLiveSpeed};
+  uint16_t run_accel{kMaxLiveAccel};
+
+  static StepControl step_controller;
+  static RotateControl rotate_controller;
+};  // namespace elevator
 
 }  // namespace elevator
 }  // namespace hiproof
